b44da68b5b1cbebca8974cb7004b2b727aff402e
Simon Richards <radishcrimson@gmail.com>
2013-12-07 14:56:52 +1300

    Get route from ecan esri server

--- unlate/src/com/unlate/MainActivity.java
+++ unlate/src/com/unlate/MainActivity.java
@@ -1,9 +1,18 @@
 package com.unlate;
 
+import java.io.InputStream;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
@@ -24,11 +33,22 @@ public class MainActivity extends Activity {
     }
     
     public void startNavigating(View view) {
-    	String uri =
-    			"https://maps.google.com/maps?saddr=-43.536,172.706&daddr=-43.48179,172.68320+to:-43.526,172.65";
-    			//"http://maps.google.com/maps?saddr="+"-43.536,172.706"+"&daddr="+"-43.526,172.65 to:43.48179,172.68320";
-    	Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(uri));
-    	intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
-    	startActivity(intent);
+    	String url = "http://arcgisdev.ecan.govt.nz/arcgis/rest/services/OSM/OSMNetwork/NAServer/Route/solve?stops=172.706%2C-43.536%3B172.68320%2C-43.48179%3B172.65%2C-43.526&barriers=&polylineBarriers=&polygonBarriers=&outSR=4326&ignoreInvalidLocations=true&accumulateAttributeNames=&impedanceAttributeName=Length&restrictionAttributeNames=Oneway&attributeParameterValues=&restrictUTurns=esriNFSBAllowBacktrack&useHierarchy=false&returnDirections=true&returnRoutes=true&returnStops=false&returnBarriers=false&returnPolylineBarriers=false&returnPolygonBarriers=false&directionsLanguage=en&directionsStyleName=&outputLines=esriNAOutputLineTrueShapeWithMeasure&findBestSequence=false&preserveFirstStop=false&preserveLastStop=false&useTimeWindows=false&startTime=0&outputGeometryPrecision=&outputGeometryPrecisionUnits=esriDecimalDegrees&directionsOutputType=esriDOTComplete&directionsTimeAttributeName=&directionsLengthUnits=esriNAUMiles&returnZ=false&f=html";
+    	HttpClient httpclient = new DefaultHttpClient();
+        HttpGet httpget = new HttpGet(url); 
+        String result = "";
+        try {
+        	HttpResponse response = httpclient.execute(httpget);
+            Log.i("Praeda",response.getStatusLine().toString());
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                InputStream instream = entity.getContent();
+        		java.util.Scanner s = new java.util.Scanner(instream).useDelimiter("\\A");
+        		if (s.hasNext()) 
+        			result = s.next();
+        	    s.close();
+                instream.close();
+            }
+        } catch (Exception e) {}
     }
 }
